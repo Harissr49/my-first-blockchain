1a8fe176b7a42acaa9af529083ce646ce94fce36
Yoosuf Mohammed Shaheed <77971881+ItsYoosuf@users.noreply.github.com>
2024-05-22 18:06:24 +0530

    Initial commit
    change

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,10 +3,4 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
-  networks: {
-    // Ganache local network configuration
-    ganache: {
-      url: "http://localhost:7545", // Default Ganache RPC server URL
-    },
-  },
 };
